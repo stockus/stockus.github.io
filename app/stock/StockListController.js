8301bb8ff0dbc7983e00d8cf16541a186eb524de
Jun Han <formulahendry@hotmail.com>
2014-11-01 21:27:47 +0800

    get stock data from quandl.com

--- app/stock/StockListController.js
+++ app/stock/StockListController.js
@@ -1,9 +1,23 @@
 //"use strict";
 
-app.controller('StockListController', function($scope) {
+app.controller('StockListController', function($scope, $http) {
+	//var idList = ["gb_msft", "gb_aapl", "gb_goog", "gb_bidu", "gb_sina", "gb_fb"];
+	var idList = ["MSFT", "AAPL", "AMZN", "IBM", "GOOG", "INTC"];
+	var startTime = new Date(new Date().setDate(new Date().getDate()-2));
+	var startDate = startTime.getFullYear() + "-" + (startTime.getMonth() + 1) + "-" + startTime.getDate();
+	var url = "https://www.quandl.com/api/v1/datasets/WIKI/";
+	var query = ".json?auth_token=EPRnixDWoq6Mnodjz8ip&trim_start="
 
-  $scope.stockList = [
-		{name:'Jani',country:'Norway'},
-		{name:'Hege',country:'Sweden'},
-		{name:'Kai',country:'Denmark'}];
+	$scope.stockList = [];
+
+	for (var i in idList) {
+		$http.get(url+idList[i]+query+startDate).
+			success(function(data, status, headers, config) {
+				$scope.stockList.push(data);
+			}).
+			error(function(data, status, headers, config) {
+				console.log(data);
+			});
+	};
+
 });
