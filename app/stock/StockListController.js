9b9adbd4fd9c3eb9f3db1b8a59312cbce7af682a
Jun Han <junhan@microsoft.com>
2014-11-03 15:12:51 +0800

    get recent 10 days data in case of weekend of holiday

--- app/stock/StockListController.js
+++ app/stock/StockListController.js
@@ -3,7 +3,7 @@
 app.controller('StockListController', function($scope, $http) {
 	//var idList = ["gb_msft", "gb_aapl", "gb_goog", "gb_bidu", "gb_sina", "gb_fb"];
 	var idList = ["MSFT", "AAPL", "AMZN", "IBM", "GOOG", "INTC"];
-	var startTime = new Date(new Date().setDate(new Date().getDate()-2));
+	var startTime = new Date(new Date().setDate(new Date().getDate()-10));
 	var startDate = startTime.getFullYear() + "-" + (startTime.getMonth() + 1) + "-" + startTime.getDate();
 	var url = "https://www.quandl.com/api/v1/datasets/WIKI/";
 	var query = ".json?auth_token=EPRnixDWoq6Mnodjz8ip&trim_start="
